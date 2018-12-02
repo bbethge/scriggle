35aa3c828cb5990c25928ba307d4b58ce11dc683
Ben Bethge <bethge931@gmail.com>
2018-12-02 12:50:17 -0600

    Use icon
    This requires the package to be installed with pip in the system
    directories or user’s default directory (probably doesn’t work on
    Windows).

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(name='Eddy',
+      version='0.1',
+      author='Ben Bethge',
+      author_email='bethge931@gmail.com',
+      description='Keyboard-only graphical text editor',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/bbethge/eddy/',
+      license='GPLv3+',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: MacOS X',
+                   'Environment :: Win32 (MS Windows)',
+                   'Environment :: X11 Applications',
+                   'Environment :: X11 Applications :: Gnome',
+                   'Environment :: X11 Applications :: GTK',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved'
+                       ' :: GNU General Public License v3 or later (GPLv3+)',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Text Editors'],
+      zip_safe=False,
+      packages=[],
+      scripts=['eddy'],
+      data_files=[('share/icons/hicolor/scalable/apps', ['eddy.svg']),
+                  ('share/applications', ['eddy.desktop'])])
